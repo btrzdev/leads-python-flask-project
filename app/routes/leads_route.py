c07ca24693a45e27a02a66f455d97416e944cf8d
Beatriz Silva <btrz.dev@gmail.com>
2022-04-24 23:36:31 -0300

    atualizando requirements

--- app/routes/leads_route.py
+++ app/routes/leads_route.py
@@ -5,5 +5,4 @@ bp_vaccination = Blueprint('bp_vaccination', __name__, url_prefix="/leads")
 
 bp_vaccination.post("")(leads_controller.create_leads)
 bp_vaccination.get("")(leads_controller.retrieve_leads)
-bp_vaccination.patch("")(leads_controller.edit_leads)
 bp_vaccination.delete("")(leads_controller.delete_leads)
